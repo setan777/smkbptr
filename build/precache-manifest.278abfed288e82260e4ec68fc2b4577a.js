b1b66dd75498dfed3a94c80196f079f30d68d678
setan777 <69381660+setan777@users.noreply.github.com>
2022-10-05 08:57:50 +0700

    Add files via upload

---
+++ build/precache-manifest.278abfed288e82260e4ec68fc2b4577a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f535bb75f7d03646dcd4565a0da6bdd",
+    "url": "/ppdbp0/index.html"
+  },
+  {
+    "revision": "d9e666763cc098914601",
+    "url": "/ppdbp0/static/css/main.c16bd3c2.chunk.css"
+  },
+  {
+    "revision": "0edb81a2424b959bca06",
+    "url": "/ppdbp0/static/js/2.2d2699c6.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/ppdbp0/static/js/2.2d2699c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e666763cc098914601",
+    "url": "/ppdbp0/static/js/main.fd2ef0f4.chunk.js"
+  },
+  {
+    "revision": "c306cd4efa82978f9983",
+    "url": "/ppdbp0/static/js/runtime-main.ebacb069.js"
+  },
+  {
+    "revision": "0ad866d6cc64f385e662c64191f9ddaa",
+    "url": "/ppdbp0/static/media/IllustratorBiodata.0ad866d6.svg"
+  },
+  {
+    "revision": "5b61a38a4f77f4a5f31746e18a542812",
+    "url": "/ppdbp0/static/media/IllustratorFront.5b61a38a.png"
+  },
+  {
+    "revision": "140912a70eca4058c381d5a1162686bc",
+    "url": "/ppdbp0/static/media/IllustratorRegister.140912a7.svg"
+  },
+  {
+    "revision": "59e9b73d739080c27775093d2a816cb3",
+    "url": "/ppdbp0/static/media/IlustratorSeleksi.59e9b73d.svg"
+  },
+  {
+    "revision": "ceb36eac1575e41eea43827a1fb73552",
+    "url": "/ppdbp0/static/media/header.ceb36eac.svg"
+  },
+  {
+    "revision": "a1b213370d82aad35f73053032bcf63f",
+    "url": "/ppdbp0/static/media/hero_illustrator.a1b21337.svg"
+  },
+  {
+    "revision": "6ba9b87f931096711ebd6ea273241722",
+    "url": "/ppdbp0/static/media/png-elementary-school-primary-education-high-school-school-class-logo-high-school-elementary-school-clipart.6ba9b87f.png"
+  },
+  {
+    "revision": "d5b4f14dcbc67ec17e0691dab62f18ef",
+    "url": "/ppdbp0/static/media/user_group_two_color.d5b4f14d.svg"
+  }
+]);
